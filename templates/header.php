34b1fd50f183d490d388f6dded1e33f24917cc61
stiller-leser <kajmossi@yahoo.de>
2023-10-13 22:43:34 +0200

    Bringing back the logo wrapper for home link

--- templates/header.php
+++ templates/header.php
@@ -39,9 +39,11 @@ endif;
 		<div class="site grid-container">
 			<header id="masthead" class="site-header grid-x grid-padding-x">
 				<div class="site-branding large-4 medium-10 small-9 cell">
-					<!-- <div class="logo-container">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-title" rel="home"><h1><i class="fas fa-wind"></i><div><?php bloginfo( 'name' ); ?></div></h1></a>
-					</div>	 -->
+					<div class="logo-container">
+						<?php if(!is_home()): ?>
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-title" rel="home"><h1>Startseite</h1></a>
+						<?php endif; ?>
+					</div>	
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation large-8 medium-2 small-3 cell">
